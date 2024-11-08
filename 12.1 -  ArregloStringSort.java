7c40933d31bdb7b9d6d97d01db41bb3cdd85f038
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2024-11-08 17:52:24 -0400

    Create 12.1 -  ArregloStringSort.java

---
+++ 12.1 -  ArregloStringSort.java
@@ -0,0 +1,41 @@
+String[] productos = new String[7];
+
+productos[0] = "Kingston Pendrive 64GB";
+productos[1] = "Samsung Galaxy";
+productos[2] = "Disco Duro SSD Samsung Externo";
+productos[3] = "Asus Notebook";
+productos[4] = "Macbook Air";
+productos[5] = "Chromecast 4ta generación";
+productos[6] = "Bicicleta Oxford";
+
+System.out.println("productos[0] = " + productos[0]);
+System.out.println("productos[1] = " + productos[1]);
+System.out.println("productos[2] = " + productos[2]);
+System.out.println("productos[3] = " + productos[3]);
+
+//Ejemplo 2
+
+String[] productos = new String[7];
+
+productos[0] = "Kingston Pendrive 64GB";
+productos[1] = "Samsung Galaxy";
+productos[2] = "Disco Duro SSD Samsung Externo";
+productos[3] = "Asus Notebook";
+productos[4] = "Macbook Air";
+productos[5] = "Chromecast 4ta generación";
+productos[6] = "Bicicleta Oxford";
+
+String prod1 = productos[0];
+String prod2 = productos[2];   
+
+String prod3 = productos[3];
+String prod4 = productos[4];
+String prod5 = productos[5];
+String prod6 = productos[6];
+
+System.out.println("productos[0] = " + prod1);   
+System.out.println("productos[1] = " + prod2);
+System.out.println("productos[2] = " + prod3);
+System.out.println("productos[3] = " + prod4);
+System.out.println("productos[4] = " + prod5);
+System.out.println("productos[5] = " + prod6);
