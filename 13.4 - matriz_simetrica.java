4cf7e52e0023cdb07e744de3ac0d7fec6cf6cd8d
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2024-11-19 12:42:29 -0400

    Create 13.4 - matriz_simetrica.java

---
+++ 13.4 - matriz_simetrica.java
@@ -0,0 +1,27 @@
+int [][] matriz = {
+  {1,2,3,4},
+  {2,1,0,5},
+  {3,0,1,6},
+  {4,5,6,7},
+};
+
+int i, j;
+i = 0;
+
+while(j < matriz.length && simetrica == true){
+    j = 0;
+    while(j < i && simetrica == true){
+        if(matriz[i][j] != matriz[j][i]){
+            simetrica = false;
+        }
+      j++;
+    }
+    i++;
+}
+
+if(simetrica){
+   System.out.println("La matriz es simétrica");
+}else{
+   System.out.println("La matriz no es simétrica");
+}
+  
