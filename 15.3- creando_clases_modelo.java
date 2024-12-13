bb8995a9d6fd313f677f23fff9fa8b22c7d800a5
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2024-12-12 20:36:41 -0400

    Create 15.3- creando_clases_modelo.java

---
+++ 15.3- creando_clases_modelo.java
@@ -0,0 +1,97 @@
+//Paquete modelo
+
+//Cliente
+package org.aguzman.appfacturas.modelo;
+
+public class Cliente {
+    private String nombre;
+    private String nif;
+
+    public String getNif() {
+        return nif;
+    }
+
+    public void setNif(String nif) {
+        this.nif = nif;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
+
+
+//Factura
+package org.aguzman.appfacturas.modelo;
+
+import java.util.Date;
+
+public class Factura {
+    private int folio;
+    private String descripcion;
+    private Date fecha;
+    private Cliente cliente;
+    private ItemFactura[] items;
+    private int indiceItems;
+    public static final int MAX_ITEMS = 10;
+    private static int ultimoFolio;
+
+    public Factura(String descripcion, Cliente cliente) {
+        this.descripcion = descripcion;
+        this.cliente = cliente;
+        this.items = new ItemFactura[MAX_ITEMS];
+        this.folio = ++ultimoFolio;
+        this.fecha = new Date();
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public int getFolio() {
+        return folio;
+    }
+
+    public void setFolio(int folio) {
+        this.folio = folio;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public ItemFactura[] getItems() {
+        return items;
+    }
+
+    public void addItem(ItemFactura item) {
+        this.items[indiceItems++] = item;
+    }
+
+}
+
+//Cliente
+
+//Cliente
+
+//Cliente
