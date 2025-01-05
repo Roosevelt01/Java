6122c6073b56bc57458104ed5a466040635d2948
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-01-04 20:49:14 -0400

    Create 17.3 - la clase_SelectForm.java
    Este código proporciona una forma de crear y administrar elementos de formulario de selección con opciones. Puede crear Opcionobjetos para definir las opciones disponibles y luego usar la SelectFormclase para crear el elemento de selección con esas opciones. El dibujarHtml()método le ayuda a generar el código HTML correspondiente para el elemento de selección.

---
+++ 17.3 - la clase_SelectForm.java
@@ -0,0 +1,90 @@
+package org.aguzman.pooclasesabstractas.form.elementos;
+
+import org.aguzman.pooclasesabstractas.form.elementos.select.Opcion;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SelectForm extends ElementoForm{
+
+    private List<Opcion> opciones;
+
+    public SelectForm(String nombre) {
+        super(nombre);
+        this.opciones = new ArrayList<Opcion>();
+    }
+
+    public SelectForm(String nombre, List<Opcion> opciones) {
+        super(nombre);
+        this.opciones = opciones;
+    }
+
+    public SelectForm addOpcion(Opcion opcion){
+        this.opciones.add(opcion);
+        return this;
+
+    }
+
+    @Override
+    public String dibujarHtml() {
+        StringBuilder sb = new StringBuilder("<select ");
+        sb.append("name='")
+                .append(this.nombre)
+                .append("'>");
+
+        for(Opcion opcion: this.opciones){
+            sb.append("\n<option value='")
+                    .append(opcion.getValor())
+                    .append("'");
+            if(opcion.isSelected()){
+                sb.append(" selected");
+            }
+            sb.append(">")
+                    .append(opcion.getNombre())
+                    .append("</option>");
+        }
+        sb.append("</select>");
+        return sb.toString();
+    }
+}
+
+package org.aguzman.pooclasesabstractas.form.elementos.select;
+
+public class Opcion {
+    private String valor;
+    private String nombre;
+    private boolean selected;
+
+    public Opcion() {
+    }
+
+    public Opcion(String valor, String nombre) {
+        this.valor = valor;
+        this.nombre = nombre;
+    }
+
+    public String getValor() {
+        return valor;
+    }
+
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public boolean isSelected() {
+        return selected;
+    }
+
+    public void setSelected(boolean selected) {
+        this.selected = selected;
+    }
+}
+
