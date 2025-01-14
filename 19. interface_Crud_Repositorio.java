5a2e80c8efa8ee7256251bab10a1870f5682687a
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-01-14 11:04:12 -0400

    Create 19. interface_Crud_Repositorio.java

---
+++ 19. interface_Crud_Repositorio.java
@@ -0,0 +1,100 @@
+//Paquete modelo
+package org.aguzman.poointerfaces.modelo;
+
+public class Cliente {
+    private Integer id;
+    private String nombre;
+    private String apellido;
+    private static int ultimoId;
+
+    public Cliente() {
+        this.id = ++ultimoId;
+    }
+
+    public Cliente(String nombre, String apellido) {
+        this();
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    @Override
+    public String toString() {
+        return "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'';
+    }
+}
+
+//Paquete repositorio
+
+//Interface CrudRespotorio
+package org.aguzman.poointerfaces.repositorio;
+
+import org.aguzman.poointerfaces.modelo.Cliente;
+
+import java.util.List;
+
+public interface CrudRepositorio {
+    List<Cliente> lista();
+    Cliente porId(Integer id);
+    void crear(Cliente cliente);
+    void edtitar(Cliente cliente);
+    void eliminar(Integer id);
+}
+
+//Enum
+package org.aguzman.poointerfaces.repositorio;
+
+public enum Direccion {
+    ASC, DESC
+}
+
+
+//Interface OrdenableRepositorio
+package org.aguzman.poointerfaces.repositorio;
+
+import org.aguzman.poointerfaces.modelo.Cliente;
+
+import java.util.List;
+
+public interface OrdenableRepositorio {
+    List<Cliente> listar(String campo, String dir);
+}
+
+//Interface PaginableRepositorio
+package org.aguzman.poointerfaces.repositorio;
+
+import org.aguzman.poointerfaces.modelo.Cliente;
+
+import java.util.List;
+
+public interface PaginableRepositorio {
+    List<Cliente> listar(int desde, int hasta);
+}
+
+
+
