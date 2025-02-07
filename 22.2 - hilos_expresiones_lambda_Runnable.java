e217933c19774e1eac2619aebe1c5863fd37760a
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-02-07 10:40:37 -0400

    Create 22.2 - hilos_expresiones_lambda_Runnable.java

---
+++ 22.2 - hilos_expresiones_lambda_Runnable.java
@@ -0,0 +1,24 @@
+package org.aguzman.hilos.ejemplos;
+
+public class EjemploInterfaceRunnableJava8 {
+    public static void main(String[] args) {
+
+        Runnable viaje = () -> {
+                for(int i = 0; i < 10;i++){
+                    System.out.println(i+ " - " + Thread.currentThread().getName());
+                    try {
+                        Thread.sleep((long)(Math.random() * 1000));
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                System.out.println("Finalmente me voy de viaje a: "+ Thread.currentThread().getName());
+
+        };
+
+        new Thread(viaje, "Isla de Pascua").start();
+        new Thread(viaje, "Robinson Crusoe").start();
+        new Thread(viaje, "Juan Fernandez").start();
+        new Thread(viaje, "Isla de Chiloe").start();
+    }
+}
