2399d6e27534ae11f62276735440735380dce322
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-02-09 18:37:55 -0400

    Create 22.8 - sincronizacion_productor_consumidor_Java8_Lambda.java

---
+++ 22.8 - sincronizacion_productor_consumidor_Java8_Lambda.java
@@ -0,0 +1,21 @@
+package org.aguzman.hilos.ejemplos.ejemplotimer;
+
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class EjemploAgendarTareaTimer {
+    public static void main(String[] args) {
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                System.out.println("Tarea realizada en: "+ new Date() + " nombre del thread: " + Thread.currentThread().getName());
+                System.out.println("Finaliza el tiempo");
+                timer.cancel();
+            }
+        },5000);
+
+        System.out.println("Agendamos una tarea para 5 minutos más...");
+    }
+}
