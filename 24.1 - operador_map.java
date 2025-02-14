21fa7a7d7029a0dbb9510564fae391a83691d619
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-02-14 18:54:24 -0400

    Update 24.1 - operador_map.java

--- 24.1 - operador_map.java
+++ 24.1 - operador_map.java
@@ -1,3 +1,36 @@
+package org.aguzman.api.stream.models;
+
+public class Usuario {
+    private String nombre, apellido;
+
+    public Usuario(String nombre, String apellido) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{nombre='" + nombre + "', apellido='" + apellido + "'}";
+    }
+}
+
+//main principal
 package org.aguzman.api.stream;
 
 
