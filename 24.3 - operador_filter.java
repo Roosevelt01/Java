f54b4e1e3446cc6272f913caaacbeabd9de500d2
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-02-14 19:36:08 -0400

    Create 24.3 - operador_filter.java

---
+++ 24.3 - operador_filter.java
@@ -0,0 +1,46 @@
+package org.aguzman.api.stream;
+
+import org.aguzman.api.stream.models.Usuario;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class EjemploStreamFilter {
+    public static void main(String[] args) {
+
+        System.out.println("Ejemplo #1");
+        Stream<Usuario> nombres = Stream
+                .of("Pato Guzman", "Paco Gonzalez", "Pepa Gutierrez", "Pepe Mena", "Pepe Garcia")
+                .map(nombre -> new Usuario(nombre.split(" ")[0], nombre.split(" ")[1]))
+                .filter(u -> u.getNombre().equals("Pepe"))
+                .peek(System.out::println);
+
+        List<Usuario> lista = nombres.collect(Collectors.toList());
+        lista.forEach(System.out::println);
+
+        //Ejemplo #2 
+        System.out.println("Ejemplo #2");
+        Stream<Usuario> nombres = Stream
+                .of("Pato Guzman", "Paco Gonzalez", "Pepa Gutierrez", "Pepe Mena", "Pepe Garcia")
+                .map(nombre -> new Usuario(nombre.split(" ")[0], nombre.split(" ")[1]))
+                .filter(u -> u.getNombre().equals("Pepe"))
+                .peek(System.out::println);
+
+        Optional<Usuario> usuario = nombres.findFirst();
+        System.out.println(usuario.get());
+
+      
+        System.out.println("Ejemplo #3");
+        Usuario usuario = Stream
+                .of("Pato Guzman", "Paco Gonzalez", "Pepa Gutierrez", "Pepe Mena", "Pepe Garcia")
+                .map(nombre -> new Usuario(nombre.split(" ")[0], nombre.split(" ")[1]))
+                .filter(u -> u.getId().equals(3))
+                .findFirst().get();
+
+        System.out.println(usuario);
+
+        
+
+    }
+}
