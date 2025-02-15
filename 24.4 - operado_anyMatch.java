46a8bc8778d68aed67c9307e61c528dfc3a33cd2
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-02-15 00:01:50 -0400

    Create 24.4 - operado_anyMatch.java

---
+++ 24.4 - operado_anyMatch.java
@@ -0,0 +1,32 @@
+package org.aguzman.api.stream;
+
+import org.aguzman.api.stream.models.Usuario;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class EjemploStreamAnymatch {
+    public static void main(String[] args) {
+
+        System.out.println("Ejemplo #1");
+        boolean existe = Stream
+                .of("Pato Guzman", "Paco Gonzalez", "Pepa Gutierrez", "Pepe Mena", "Pepe Garcia")
+                .map(nombre -> new Usuario(nombre.split(" ")[0], nombre.split(" ")[1]))
+                .peek(System.out::println)
+                .anyMatch(u -> u.getId().equals(4));
+
+        System.out.println(existe);
+
+        System.out.println("\nEjemplo #2");
+        Long count = Stream
+                .of("Pato Guzman", "Paco Gonzalez", "Pepa Gutierrez", "Pepe Mena", "Pepe Garcia")
+                .map(nombre -> new Usuario(nombre.split(" ")[0], nombre.split(" ")[1]))
+                .peek(System.out::println)
+                .count();
+
+        System.out.println(count );
+
+    }
+}
