a13a31715ec5052dc475268122f314735d779321
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-02-16 16:34:42 -0400

    Update 24.7 - operadores.java

--- 24.7 - operadores.java
+++ 24.7 - operadores.java
@@ -1,3 +1,62 @@
+//Clase Usuario
+package org.aguzman.api.stream.models;
+
+import java.util.Objects;
+
+public class Usuario {
+    private String nombre, apellido;
+    private Integer id;
+    private static int ultimoId;
+
+    public Usuario(String nombre, String apellido) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.id = ++ultimoId;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{nombre='" + nombre + "', apellido='" + apellido + "'}";
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (object == null || getClass() != object.getClass()) return false;
+        Usuario usuario = (Usuario) object;
+        return Objects.equals(nombre, usuario.nombre) && Objects.equals(apellido, usuario.apellido);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nombre, apellido);
+    }
+}
+
+//Main principal
 package org.aguzman.api.stream;
 
 import org.aguzman.api.stream.models.Usuario;
