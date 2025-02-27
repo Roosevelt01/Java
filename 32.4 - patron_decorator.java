f1b41f606616fd864f4337f3b6f73f44475211ac
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-02-27 02:25:27 -0400

    Create 32.4 - patron_decorator.java

---
+++ 32.4 - patron_decorator.java
@@ -0,0 +1,156 @@
+//Interface Formatable
+package decorator;
+
+public interface Formateable {
+    String darFormato();
+}
+
+//Clase texto
+package decorator;
+
+public class Texto implements Formateable{
+    private String texto;
+
+    public Texto(String texto) {
+        this.texto = texto;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    @Override
+    public String darFormato() {
+        return getTexto();
+    }
+}
+
+//Clase TextoDecorador
+package decorator.decorador;
+
+import decorator.Formateable;
+
+public abstract class TextoDecorador implements Formateable {
+    protected Formateable texto;
+
+        public TextoDecorador(Formateable texto) {
+            this.texto = texto;
+        }
+}
+
+//MayusculaDecorador
+package decorator.decorador;
+
+import decorator.Formateable;
+
+public class MayusculaDecorador extends TextoDecorador {
+    public MayusculaDecorador(Formateable texto) {
+        super(texto);
+    }
+
+    @Override
+    public String darFormato() {
+        return texto.darFormato().toUpperCase();
+    }
+}
+
+//MayusculaDecorator
+package decorator.decorador;
+
+import decorator.Formateable;
+
+public class MayusculaDecorador extends TextoDecorador {
+    public MayusculaDecorador(Formateable texto) {
+        super(texto);
+    }
+
+    @Override
+    public String darFormato() {
+        return texto.darFormato().toUpperCase();
+    }
+}
+
+//Clase ReversaDecorador
+package decorator.decorador;
+
+import decorator.Formateable;
+
+public class ReversaDecorador extends TextoDecorador{
+    public ReversaDecorador(Formateable texto) {
+        super(texto);
+    }
+
+    @Override
+    public String darFormato() {
+        StringBuilder sb = new StringBuilder(texto.darFormato());
+        return sb.reverse().toString();
+    }
+}
+
+//Clase RemplazarEspaciosDecorador
+package decorator.decorador;
+
+import decorator.Formateable;
+
+public class RemplazarEspaciosDecorador extends TextoDecorador{
+    public RemplazarEspaciosDecorador(Formateable texto) {
+        super(texto);
+    }
+
+    @Override
+    public String darFormato() {
+        return texto.darFormato().replace(" ","_");
+    }
+}
+
+//Clase RemplazarEspaciosDecorador
+package decorator.decorador;
+
+import decorator.Formateable;
+
+public class SubrayadoDecorador extends TextoDecorador{
+    public SubrayadoDecorador(Formateable texto) {
+        super(texto);
+    }
+
+    @Override
+    public String darFormato() {
+        int largo = texto.darFormato().length();
+        StringBuilder sb = new StringBuilder(texto.darFormato());
+        sb.append("\n");
+        for (int i = 0; i < largo;i++){
+            sb.append("_");
+        }
+        return sb.toString();
+    }
+}
+
+//Clase SubrayadoDecorador
+package decorator.decorador;
+
+import decorator.Formateable;
+
+public class SubrayadoDecorador extends TextoDecorador{
+    public SubrayadoDecorador(Formateable texto) {
+        super(texto);
+    }
+
+    @Override
+    public String darFormato() {
+        int largo = texto.darFormato().length();
+        StringBuilder sb = new StringBuilder(texto.darFormato());
+        sb.append("\n");
+        for (int i = 0; i < largo;i++){
+            sb.append("_");
+        }
+        return sb.toString();
+    }
+}
+
+//Main principal
+
+    
+
+
+
+  
