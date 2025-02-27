b868b56f5c5b4727316dcde5431678bb8cb8a828
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-02-27 02:58:08 -0400

    Update 32.4 - patron_decorator.java

--- 32.4 - patron_decorator.java
+++ 32.4 - patron_decorator.java
@@ -148,9 +148,20 @@ public class SubrayadoDecorador extends TextoDecorador{
 }
 
 //Main principal
+package decorator.decorador;
 
-    
+import decorator.Formateable;
+import decorator.Texto;
 
+public class EjemploDecorador {
+    public static void main(String[] args) {
+        Formateable texto = new Texto("Hola que tal Andrés!");
 
+        MayusculaDecorador mayuscula = new MayusculaDecorador(texto);
+        ReversaDecorador reversa = new ReversaDecorador(mayuscula);
+        SubrayadoDecorador subrayar = new SubrayadoDecorador(reversa);
+        RemplazarEspaciosDecorador remplazar = new RemplazarEspaciosDecorador(subrayar);
 
-  
+        System.out.println(subrayar.darFormato());
+    }
+}
