e451fcfc8790504e41af50854c33bec60b3d900e
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-03-02 16:24:24 -0400

    Update 32.5 - patrónComposite.java

--- 32.5 - patrónComposite.java
+++ 32.5 - patrónComposite.java
@@ -59,8 +59,11 @@ public class Directorio extends Componente{
                 .append("\n");
 
         for(Componente hijo: this.hijos){
-            sb.append(hijo.mostrar())
-                    .append("\n");
+            sb.append(hijo.mostrar());
+            if(hijo instanceof  Archivo){
+                sb.append("\n");
+            }
+
         }
 
         return sb.toString();
@@ -68,3 +71,30 @@ public class Directorio extends Componente{
 
 
 }
+
+//
+
+import org.aguzman.patrones.composite.Archivo;
+import org.aguzman.patrones.composite.Directorio;
+
+public class ejemploComposite {
+    public static void main(String[] args) {
+        Directorio doc = new Directorio("Documentos");
+        Directorio java = new Directorio("Java");
+
+        java.addComponente(new Archivo("patron-composite.docx"));
+        Directorio stream = new Directorio("Api Stream");
+        stream.addComponente(new Archivo("stream-map.docx"));
+
+        java.addComponente(stream);
+
+        doc.addComponente(java);
+        doc.addComponente(new Archivo("cv.docx"));
+        doc.addComponente(new Archivo("logo.jpeg"));
+
+        System.out.println(doc.mostrar());
+    }
+}
+
+
+
