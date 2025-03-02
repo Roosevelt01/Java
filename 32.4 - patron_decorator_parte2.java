0c4f67d35d1695e66ad5d51592d1625e2004cf51
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-03-02 11:14:23 -0400

    Update 32.4 - patron_decorator_parte2.java

--- 32.4 - patron_decorator_parte2.java
+++ 32.4 - patron_decorator_parte2.java
@@ -86,4 +86,57 @@ public class ConCremaDecorador extends CafeDecorador{
     }
 }
 
+//
+package decorator.decorador;
+
+import decorator.Configurable;
+
+public class LecheConDecorador extends CafeDecorador {
+    public LecheConDecorador(Configurable cafe) {
+        super(cafe);
+    }
+
+    @Override
+    public float getPrecioBase() {
+        return cafe.getPrecioBase() + 3.7f;
+    }
+
+    @Override
+    public String getIngredientes() {
+        return cafe.getIngredientes() + ", Leche";
+    }
+}
+
+//Main Principal
+package decorator.decorador;
+
+import decorator.Cafe;
+import decorator.Configurable;
+
+public class EjemploDecoradorCafe {
+    public static void main(String[] args) {
+        Configurable cafe = new Cafe(7, "Cafe Mocha");
+        ConCremaDecorador conCrema = new ConCremaDecorador(cafe);
+        LecheConDecorador conLeche = new LecheConDecorador(conCrema);
+        ConChocolateDecorador conChoco = new ConChocolateDecorador(conLeche);
+
+        System.out.println("El precio del café mosca es: " + conChoco.getPrecioBase());
+        System.out.println("Los ingredientes del café mosca es: " + conChoco.getIngredientes());
+
+        Configurable capuchino = new Cafe(4, "Cafe Capuchino");
+        conCrema = new ConCremaDecorador(capuchino);
+        conLeche = new LecheConDecorador(conCrema);
+        System.out.println("\nEl precio del capuchino es: " + conLeche.getPrecioBase());
+        System.out.println("Los ingredientes del capuchino es: " + conLeche.getIngredientes());
+
+        Configurable espresso = new Cafe(3, "Cafe Espresso");
+        System.out.println("\nEl precio del café Espresso es: " + espresso.getPrecioBase());
+        System.out.println("Los ingredientes del café Espresso es: " + conLeche.getIngredientes());
+
+    }
+}
+
+
+
+
 
