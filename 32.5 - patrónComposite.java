093d287dc2d6442658997bcb6cd5a088920c726e
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-03-02 15:15:41 -0400

    Update 32.5 - patrónComposite.java

--- 32.5 - patrónComposite.java
+++ 32.5 - patrónComposite.java
@@ -29,3 +29,42 @@ public abstract class Componente {
 }
 
 //
+package org.aguzman.patrones.composite;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Directorio extends Componente{
+    private List<Componente> hijos;
+
+    public Directorio(String nombre) {
+        super(nombre);
+        this.hijos=new ArrayList<>();
+    }
+
+    public Directorio addComponente(Componente c){
+        hijos.add(c);
+        return this;
+    }
+
+    public void  removeComponente(Componente c){
+        hijos.remove(c);
+    }
+
+    @Override
+    public String mostrar() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(nombre)
+                .append("/")
+                .append("\n");
+
+        for(Componente hijo: this.hijos){
+            sb.append(hijo.mostrar())
+                    .append("\n");
+        }
+
+        return sb.toString();
+    }
+
+
+}
