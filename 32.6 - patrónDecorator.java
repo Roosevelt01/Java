8199d0e08ae19926f55f204d8d6faeb1b14c6fa0
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-03-03 03:38:30 -0400

    Create 32.6 - patrónDecorator.java

---
+++ 32.6 - patrónDecorator.java
@@ -0,0 +1,55 @@
+//
+package org.aguzman.patrones.observer;
+
+public interface Observer {
+    void update(Observable observable);
+}
+
+package org.aguzman.patrones.observer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Observable {
+    protected List<Observer> observers = new ArrayList<>();
+
+    public void addObserver(Observer observer){
+        observers.add(observer);
+    }
+
+    public void remove(Observer observer){
+        observers.remove(observer);
+    }
+
+    public void notifyObserver(){
+        for(Observer obs: this.observers){
+            obs.update(this);
+        }
+    }
+}
+
+//
+package org.aguzman.patrones.observer;
+
+public class Coporacion extends Observable{
+    private String nombre;
+    private int precio;
+
+    public Coporacion(String nombre, int precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void modificarPrecio(int precio){
+        this.precio = precio;
+        notifyObserver();
+    }
+}
