6543082323d7bedab6caca0a19759bafb2f06c6a
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-03-04 20:13:06 -0400

    Update 32.6 - patrónDecorator.java

--- 32.6 - patrónDecorator.java
+++ 32.6 - patrónDecorator.java
@@ -1,10 +1,11 @@
-//
+//Clase Observer (Observador)
 package org.aguzman.patrones.observer;
 
 public interface Observer {
     void update(Observable observable);
 }
 
+//Clase Observable (Sujeto):
 package org.aguzman.patrones.observer;
 
 import java.util.ArrayList;
@@ -28,7 +29,7 @@ public abstract class Observable {
     }
 }
 
-//
+//Clase Coporacion (Sujeto Concreto):
 package org.aguzman.patrones.observer;
 
 public class Coporacion extends Observable{
@@ -52,31 +53,35 @@ public class Coporacion extends Observable{
         this.precio = precio;
         notifyObserver();
     }
-}
 
-//
-package org.aguzman.patrones.observer;
-
-public class Coporacion extends Observable{
-    private String nombre;
-    private int precio;
-
-    public Coporacion(String nombre, int precio) {
-        this.nombre = nombre;
-        this.precio = precio;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public int getPrecio() {
-        return precio;
-    }
-
-    public void modificarPrecio(int precio){
-        this.precio = precio;
-        notifyObserver();
+    @Override
+    public String toString() {
+        return getNombre() +
+                " nuevo precio $" +
+                getPrecio();
     }
 }
 
+//Clase EjemploObserver (Observadores Anónimos)
+package org.aguzman.patrones.observer.Ejemplo;
+
+import org.aguzman.patrones.observer.Corporacion;
+
+public class EjemploObserver {
+    public static void main(String[] args) {
+        Corporacion google = new Corporacion("Google", 1000);
+        google.addObserver(observable -> {
+            System.out.println("John: "+observable);
+        });
+
+        google.addObserver(observable -> {
+            System.out.println("Andrés: "+ observable);
+        });
+
+        google.addObserver(observable -> {
+            System.out.println("María: " + observable);
+        });
+
+        google.modificarPrecio(2000);
+    }
+}
