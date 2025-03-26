3f7a2f2a6b81c780d99efafe8c3758e3e6fe315c
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-03-26 03:23:19 -0400

    java

---
+++ 34 - Sección 34: Base de datos con JDBC/7. Implementando la clase Repositorio parte 3 el CRUD/repositorio/ProductoRepositorioImpl.java
@@ -0,0 +1,98 @@
+package org.aguzman.java.jdbc.repositorio;
+
+import org.aguzman.java.jdbc.modelo.Producto;
+import org.aguzman.java.jdbc.util.ConexionBaseDatos;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductoRepositorioImpl implements Repositorio<Producto>{
+
+    private Connection getConnection() throws SQLException {
+        return ConexionBaseDatos.getInstance();
+    }
+
+    @Override
+    public List<Producto> listar() {
+        List<Producto> productos = new ArrayList<>();
+
+        try (Statement stmt = getConnection().createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM productos")){
+            while (rs.next()){
+                Producto p = crearProducto(rs);//Recien agregado en esa clase
+                productos.add(p);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return productos;
+    }
+
+    @Override
+    public Producto porId(Long id) {
+        Producto producto = null;
+
+        try(PreparedStatement stmt = getConnection().
+            prepareStatement("SELECT * FROM productos WHERE id = ?")) {
+            stmt.setLong(1,id);
+
+            //Codigo modificado en esa clase
+            try (ResultSet rs = stmt.executeQuery()) {
+                if (rs.next()) {
+                    producto = crearProducto(rs);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return producto;
+    }
+
+    //Codigo modificado en esa clase
+    @Override
+    public void guardar(Producto producto) {
+        String sql;
+        if (producto.getId() != null && producto.getId() > 0) {
+            sql = "UPDATE productos SET nombre=?, precio=? WHERE id=?";
+        } else {
+            sql = "INSERT INTO productos(nombre, precio, fecha_registro) VALUES(?,?,?)";
+        }
+
+        try(PreparedStatement stmt = getConnection().prepareStatement(sql)){
+            stmt.setString(1, producto.getNombre());
+            stmt.setLong(2, producto.getPrecio());
+
+            if (producto.getId() != null && producto.getId() > 0) {
+                stmt.setLong(3, producto.getId());
+            }else{
+                stmt.setDate(3, new Date( producto.getFechaRegistro().getTime()));
+            }
+
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    //Codigo modificado en esa clase
+    @Override
+    public void eliminar(Long id) {
+        try(PreparedStatement stmt = getConnection().prepareStatement("DELETE FROM productos WHERE id=?")){
+            stmt.setLong(1, id);
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static Producto crearProducto(ResultSet rs) throws SQLException {
+        Producto p = new Producto();
+        p.setId(rs.getLong("id"));
+        p.setNombre(rs.getString("nombre"));
+        p.setPrecio(rs.getInt("precio"));
+        p.setFechaRegistro(rs.getDate("fecha_registro"));
+        return p;
+    }
+}
